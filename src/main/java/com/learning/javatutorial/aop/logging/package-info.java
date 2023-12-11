b64d94c9ea4ac59616d7ae7c98037eab55f02b39
assem.salem@dafa.sa <assem.salem@dafa.sa>
2023-12-11 09:48:03 +0200

    Initial version of java generated by generator-jhipster@8.0.0

---
+++ src/main/java/com/learning/javatutorial/aop/logging/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Logging aspect.
+ */
+package com.learning.javatutorial.aop.logging;
---
+++ src/main/java/com/learning/javatutorial/web/rest/errors/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer error handling.
+ */
+package com.learning.javatutorial.web.rest.errors;
